f5fe7097e8c7bd47f232e3c325897b8b498691e8
Simas Skilinskas <simas.skilinskas@devbridge.com>
2020-05-05 10:06:09 +0300

    scss modules, svg as element samples

--- src/App.js
+++ src/App.js
@@ -1,17 +1,17 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { ReactComponent as Logo } from './logo.svg';
+import styles from './App.module.scss';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <div className={styles.App}>
+      <header className={styles.AppHeader}>
+        <Logo className={styles.AppLogo}/>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
-          className="App-link"
+          className={styles.AppLink}
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
