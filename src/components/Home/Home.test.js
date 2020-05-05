68f7533d9b2943bdbfee2fdcee2358ffdf5df479
Simas Skilinskas <simas.skilinskas@devbridge.com>
2020-05-05 18:37:41 +0300

    Test fix

--- src/components/Home/Home.test.js
+++ src/components/Home/Home.test.js
@@ -1,9 +1,14 @@
 import React from "react";
 import { render } from "@testing-library/react";
 import Home from "./Home";
+import { BrowserRouter } from "react-router-dom";
 
 test("renders learn react link", () => {
-  const { getByText } = render(<Home />);
+  const { getByText } = render(
+    <BrowserRouter>
+      <Home />
+    </BrowserRouter>
+  );
   const linkElement = getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
